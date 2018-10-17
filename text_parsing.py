f42fc46eeb036cc65e997c9d87c9e8fa653745e3
Lillianwin <lillianwin.z@gmail.com>
2018-10-17 16:46:13 -0400

    Update: created dummy variables for genres

--- text_parsing.py
+++ text_parsing.py
@@ -7,11 +7,9 @@ Created on Mon Oct 15 15:13:40 2018
 """
 import os
 import gensim as ge
-import nltk
-import re
 import string as st
 import pandas as pd
-from nltk.corpus import stopwords
+
 
 os.getcwd()
 os.listdir()
@@ -19,14 +17,18 @@ os.listdir()
 # Create initial documents list
 df = pd.read_excel("IMDB_Review_data.xlsx")
 print(df.head())
+
 print(df['Review'].head())
 type(df['Review'].head()) # one column is a pd series
 df.shape
 
 # remove rows with missing values in Review column
 df = df.dropna(subset=['Review'])
-
+df = df.dropna(subset=['Genre'])
 doc = df['Review']
+
+df["Rating"].value_counts()
+
 
 def text_parsing (doc):
     """
@@ -37,10 +39,11 @@ def text_parsing (doc):
     import re
     from nltk.corpus import stopwords
     import nltk
+    import string as st
     
     # Remove punctuation, then tokenize documents
     
-    punc = re.compile( '[%s]' % re.escape( string.punctuation ) )
+    punc = re.compile( '[%s]' % re.escape( st.punctuation ) )
     term_vec = [ ]
     
     for d in doc:
@@ -79,7 +82,59 @@ term_parsed = text_parsing(doc)
 len(term_parsed)
 
 df["term_parsed"] = term_parsed
-df.head()
+#df.head()
+#type(df["Genre"][10])
 
 
+# Convert the rows in string format of genre column to lists.
+df["Genre"][1] # every entry is a string enclosed with quotes
+import ast
+col_genre = []
+for x in df["Genre"]:
+    try:
+        x = ast.literal_eval(x)
+        col_genre.append(x)
+    except ValueError:
+        l = []
+        l.append(x)
+        col_genre.append(l)
+len(col_genre)
 
+df["Genre"] = col_genre
+
+df["Genre"][1] # now it's a list
+
+                                                                                 
+# get top 10 genres and make binary variables to identify the genres a review belongs to
+# Comedy Drama Horror Action Thriller Romance Sci-Fi Fantasy Adventure Documentary...
+
+type(genre["type"][10])
+list(genre["type"][10])
+
+genre_dict = {}
+for row in df["Genre"]:
+    
+    for item in row:
+        if item not in genre_dict:
+            genre_dict[item] = 1
+        else:
+            genre_dict[item] +=1
+
+genre_dict
+len(genre_dict)
+
+type(df["Genre"])
+
+# create dummy variables for all genres and concatenate to make a final dataframe
+dummy = pd.get_dummies(df["Genre"].apply(pd.Series).stack(),prefix='g').sum(level=0) 
+
+dummy.shape
+df.shape
+dummy.tail(10)
+
+df_final = pd.concat([df,dummy], axis=1)
+df_final.shape
+# double check
+len(df_final.loc[df_final["g_Comedy"] == 1])
+
+df_final.to_csv("data_final.csv")
